089765d1da130560e253f2b6783a394c17d152ce
Bethelhem <bethelhem.tadele@gmail.com>
2016-02-02 10:40:14 -0800

    Update my_solution.rb

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -40,11 +40,13 @@ die.roll
 
 # What were the main differences between this die class and the last one you created in terms of implementation? Did you need to change much logic to get this to work?
 
-#    In the previous die class, I provided the value of the @sides but in this exercise the sides takes the length of the labels and it randomly select a sample from the list of lables provided and print it when roll is called.
+#    In the previous die class, I provided the value of the @sides but in this exercise the sides takes the length of the 
+# labels and it randomly select a sample from the list of lables provided and print it when roll is called.
 
 # What does this exercise teach you about making code that is easily changeable or modifiable?
 
-# classes help to make modifiable code because they force us to break the problem into methods in which we can write or read from easily
+# classes help to make modifiable code because they force us to break the problem into methods in which we can write 
+# or read from easily
 
 # What new methods did you learn when working on this challenge, if any?
 
@@ -52,6 +54,7 @@ die.roll
 
 # What concepts about classes were you able to solidify in this challenge?
 
-# I learned that when I create a new method with the intention of chaining it with another, I should write the name of my next method immediately before the end.
+# I learned that when I create a new method with the intention of chaining it with another, 
+# I should write the name of my next method immediately before the end.
 
 
