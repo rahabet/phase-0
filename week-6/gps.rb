86c6b1ddcf28eb3a12e1b92a7d6ecd65c61cf6b5
Bethelhem <bethelhem.tadele@gmail.com>
2016-02-01 20:06:42 -0800

    Update gps.rb

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -70,7 +70,8 @@ p serving_size_calc("THIS IS AN ERROR", 5)
 
 # What did you learn about making code readable by working on this challenge?
 
-# It was challenging for me because i barely write my own code and refactoring someone elses code is like confusing. but we manged to understand what it does and able to make it a more readable.
+# It was challenging for me because i barely write my own code and refactoring someone elses code is like confusing. 
+# but we manged to understand what it does and able to make it a more readable.
 
 # Did you learn any new methods? What did you learn about them?
 
@@ -78,8 +79,10 @@ p serving_size_calc("THIS IS AN ERROR", 5)
 
 # What did you learn about accessing data in hashes?
 
-# by using the key or the value, we can easily access hashes and also to see whether somehting is included in the hash we can simply use has_key method.
+# by using the key or the value, we can easily access hashes and also to see whether somehting is included in the hash we 
+# can simply use has_key method.
 
 # What concepts were solidified when working through this challenge?
 
-# I see the point why we need to write pseudocode and also comments because that way another reader easily understand what our thinking process is. other wise it takes more time to understand what is happenining.
+# I see the point why we need to write pseudocode and also comments because that way another reader easily understand what
+# our thinking process is.other wise it takes more time to understand what is happenining.
